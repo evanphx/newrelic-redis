0cf2b7dadb2b05635dfaea271df6e862c49650af
Jason R. Clark <jclark@newrelic.com>
2015-03-16 14:15:40 -0700

    Update tests to work with latest agent
    A number of different methods which have been deprecated or removed were
    in use in the tests. This brings them up to date with the 3.11 agent
    practices we'd like. Further work to update to full Datastores support
    for New Relic will be built on top of this.

--- test/test_newrelic_redis.rb
+++ test/test_newrelic_redis.rb
@@ -1,61 +1,63 @@
 require 'test/unit'
 require 'redis'
 
+require 'newrelic_rpm'
 require 'newrelic_redis/instrumentation'
 
-class TestNewRelicRedis < Test::Unit::TestCase
-  include NewRelic::Agent::Instrumentation::ControllerInstrumentation
+DependencyDetection.detect!
 
+NewRelic::Agent.require_test_helper
+
+class TestNewRelicRedis < Test::Unit::TestCase
   PORT = 6381
   OPTIONS = {:port => PORT, :db => 15, :timeout => 0.1}
 
   def setup
-    NewRelic::Agent.manual_start
-    @engine = NewRelic::Agent.instance.stats_engine
-    @engine.clear_stats
-
-    @sampler = NewRelic::Agent.instance.transaction_sampler
-    @sampler.enable
-    @sampler.reset!
-    @sampler.start_builder
+    NewRelic::Agent.drop_buffered_data
 
     @redis = Redis.new OPTIONS
     @client = @redis.client
-
-    DependencyDetection.detect!
-  end
-
-  def teardown
-    @sampler.clear_builder
   end
 
   def assert_metrics(*m)
-    m.each do |x|
-      assert @engine.metrics.include?(x), "#{x} not in metrics"
-    end
+    assert_metrics_recorded(m)
+  end
+
+  def assert_segment_has_key(segment_name, expected)
+    sample = NewRelic::Agent.agent.transaction_sampler.tl_builder.sample
+    segment = find_segment_with_name(sample, segment_name)
+    assert_equal expected, segment.params[:key]
   end
 
   def test_call
-    @redis.hgetall "foo"
-    assert_metrics "Datastore/Redis/HGETALL", "Datastore/Redis/allOther"
+    with_config(:'transaction_tracer.record_sql' => 'raw') do
+      in_transaction do
+        @redis.hgetall "foo"
+        assert_segment_has_key "Datastore/Redis/SELECT", "[[:select, 15]]"
+        assert_segment_has_key "Datastore/Redis/HGETALL", "[[:hgetall, \"foo\"]]"
+      end
+    end
 
-    prm = @sampler.builder.current_segment.params
-    assert_equal "[[:select, 15]];\n[[:hgetall, \"foo\"]]", prm[:key]
+    assert_metrics "Datastore/Redis/HGETALL", "Datastore/Redis/allOther"
   end
 
   def test_call_pipelined
-    @redis.pipelined do
-      @redis.hgetall "foo"
-      @redis.incr "bar"
+    with_config(:'transaction_tracer.record_sql' => 'raw') do
+      in_transaction do
+        @redis.pipelined do
+          @redis.hgetall "foo"
+          @redis.incr "bar"
+        end
+
+        assert_segment_has_key "Datastore/Redis/SELECT", "[[:select, 15]]"
+        assert_segment_has_key "Datastore/Redis/Pipelined", "[[:hgetall, \"foo\"], [:incr, \"bar\"]]"
+      end
     end
 
     assert_metrics "Datastore/Redis/Pipelined",
                    "Datastore/Redis/Pipelined/HGETALL",
                    "Datastore/Redis/Pipelined/INCR",
                    "Datastore/Redis/allOther"
-
-    prm = @sampler.builder.current_segment.params
-    assert_equal "[[:select, 15]];\n[[:hgetall, \"foo\"], [:incr, \"bar\"]]", prm[:key]
   end
 
   def test_call_with_block
@@ -73,18 +75,21 @@ class TestNewRelicRedis < Test::Unit::TestCase
   end
 
   def test_obfuscated
-    NewRelic::Control.instance["transaction_tracer.record_sql"] = "obfuscated"
-    @redis.pipelined do
-      @redis.hgetall "foo"
-      @redis.incr "bar"
+    with_config(:'transaction_tracer.record_sql' => 'obfuscated') do
+      in_transaction do
+        @redis.pipelined do
+          @redis.hgetall "foo"
+          @redis.incr "bar"
+        end
+
+        assert_segment_has_key "Datastore/Redis/SELECT", "[[:select, \"?\"]]"
+        assert_segment_has_key "Datastore/Redis/Pipelined", "[[:hgetall, \"?\"], [:incr, \"?\"]]"
+      end
     end
 
     assert_metrics "Datastore/Redis/Pipelined",
                    "Datastore/Redis/Pipelined/HGETALL",
                    "Datastore/Redis/Pipelined/INCR",
                    "Datastore/Redis/allOther"
-
-    prm = @sampler.builder.current_segment.params
-    assert_equal "[[:select, \"?\"]];\n[[:hgetall, \"?\"], [:incr, \"?\"]]", prm[:key]
   end
 end
